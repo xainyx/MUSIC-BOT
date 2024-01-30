f08fc074b34980ed80595838b127ff0c25684329
xainyx <136616612+xainyx@users.noreply.github.com>
2024-01-30 13:03:59 +0100

    Update playSong.js

--- events/player/playSong.js
+++ events/player/playSong.js
@@ -9,12 +9,12 @@ module.exports = async (client, queue, song) => {
       .setAuthor({
         name: 'Currently playing a Track',
         iconURL: 'https://cdn.discordapp.com/attachments/1140841446228897932/1144671132948103208/giphy.gif', 
-        url: 'https://discord.gg/FUEHs7RCqz'
+        url: 'https://discord.gg/refanxo'
     })
     .setDescription(`\n ‎ \n▶️ **Details :** **${song?.name}**\n▶️ **Enjoy the Ultimate Music Experience. ** \n▶️ **If link breaks playback try to give query.**`)
 .setImage(queue.songs[0].thumbnail)
     .setColor('#FF0000')
-    .setFooter({ text: 'More info - Use /help command [RTX GAMING]' });
+    .setFooter({ text: 'More info - Use /help command' });
      
       queue?.textChannel?.send({ embeds: [embed] }).catch(e => { });
     }
